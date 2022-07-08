da93f648141907a66b179794fa4339bc1db180ed
denist2322 <parksomang125@gmail.com>
2022-07-08 09:08:37 +0900

    useEffect도 조건을 주면 변화 가능

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
     console.log("Call The Api....");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword & counter' changes.");
+  }, [keyword, counter]);
+  // useEffect는 한번만 실행되지만,
+  // 위와같이 정의해주면 keyword가 변화하면 실행됨
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
